3bff6c6814c1a36682000208417b94468765cea1
jGavranovic <jovangavranovic4@gmail.com>
2024-03-12 15:14:27 -0400

    js

--- SOEN287/Class/test.js
+++ SOEN287/Class/test.js
@@ -1,17 +1,30 @@
-var scope = "global scope";
-function checkscope(){
-    var scope = "local scope";
-    function nested(){
-        var scope = "nested scope";
-        alert("1-" + scope);
-    }
-    nested();
-    // scope = "local scope";
-    // for(var i=0;i<10;i++){
-    //     var scope = "nested scope";
-    // }
-    alert("2-"+scope);
-}
+// function plane(make, model, year){
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+// }
+class plane {
+    make;
+    model;
+    year;
 
-checkscope();
-alert("3-"+scope);
+    constructor(make, model,year){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+}
+let plane1 = new plane("Boeing", "747", 2000);
+
+plane1.color = "red";
+
+plane.prototype.price = 1000;
+
+
+
+for (var variable in plane1){
+    document.write(variable +": "+plane1[variable]+"<br>");
+}
+plane.prototype.mileage = function(){return (2024-this.year)*1000};
+
+document.write(plane1.mileage());
